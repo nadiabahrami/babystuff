a52858d8d301981f065075949fb7f5a3e7e88ef0
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-01-25 10:01:25 -0800

    Keys

---
+++ scripts/keys.js
@@ -0,0 +1,10 @@
+(function(module){
+  var key = {};
+
+  key.walmartKey = 'nzqhwruahscwc5ra9kb5uzb6';
+  key.amazonKey = 'AKIAISTA6ZY7WRIH6TRQ';
+  key.searsKey = '';
+  key.recallKey = 'ddcde1b6-ebca-4bdd-bdb8-d8a2d9f40f17';
+
+  module.key=key;
+})(window);
