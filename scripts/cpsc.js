1b713f9aa581fb830b308c2beb1124b59a500124
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-01-26 09:19:25 -0800

    cleaned up cpsc.js and created resultsView.js

--- scripts/cpsc.js
+++ scripts/cpsc.js
@@ -1,6 +1,9 @@
 (function(module){// search recalls
   var cpsc = {};
   cpsc.all = [];
+  cpsc.newUPC = [];
+
+  cpsc.editted = [];
 
   cpsc.getRecalls = function() {
     console.log("between funtions");
@@ -8,7 +11,8 @@
       if (data != null) {
         console.log(data);
         cpsc.all = data;
-        console.log(returned.length);
+        cpsc.editted =data;
+        console.log(cpsc.all.length);
       }else{
         alert('Recall database unavailable!');
       }
@@ -16,54 +20,49 @@
   };
 
 
-
-  .done(function(){
-
-var newUPC = [];
-
-for(i=0; i<returned.length; i++){
-  if(returned[i].ProductUPCs.length !==0){
-    newUPC.push(returned[i]);
-    returned.splice(returned[i], 1);
-    i--;
-  }
-};
-console.log(newUPC.length);
-console.log(returned.length);
-}).done(function (){
-  var descriptionUPC = [];
-  for(i=0; i<returned.length; i++){
-    if(returned[i].Description.match(/\b\d{12}\b/g) && returned[i].Description.match(/UPC/)){
-      //store REGEX search for others and store into object
-      descriptionUPC.push(returned[i]);
-      returned.splice(returned[i], 1);
-      i--;
-    }
+  cpsc.ProductUPC = function(x){
+    x.forEach(function(y){
+      if(y.ProductUPCs.length !== 0){
+        console.log("found ProductUPC");
+        cpsc.newUPC.push(y);
+        x.splice(x.indexOf(y), 1);
+      }
+    });
+    console.log(cpsc.newUPC.length);
+    console.log(cpsc.newUPC);
+    console.log(x.length);
+    console.log(x);
+    cpsc.all = x;
   };
-console.log(descriptionUPC.length);
-console.log(returned.length);
-console.log(descriptionUPC);
-console.log(returned);
-})
 
-};
-//  $.ajax({
-//  url: 'http://www.saferproducts.gov/RestWebServices/Recall?ProductName=Toddler&format=json',
-//  type: 'GET',
-//  datatype: 'json',
-//  headers: { 'Access-Control-Allow-Origin': 'ddcde1b6-ebca-4bdd-bdb8-d8a2d9f40f17' },
-//  error:
-//  function (jqXHR, textStatus, errorThrown) {
-//  var whatError = errorThrown;
-// alert(whatError);
-//  },
-//  success: function (data) {
-//    if (data != null) {
-//      displayRecallsTable(data);
-//    }else{
-//      alert('null!');
-//    }
-// }});
+//   .done(function(){
+//
+// var newUPC = [];
+//
+// for(i=0; i<returned.length; i++){
+//   if(returned[i].ProductUPCs.length !==0){
+//     newUPC.push(returned[i]);
+//     returned.splice(returned[i], 1);
+//     i--;
+//   }
+// };
+// console.log(newUPC.length);
+// console.log(returned.length);
+// }).done(function (){
+//   var descriptionUPC = [];
+//   for(i=0; i<returned.length; i++){
+//     if(returned[i].Description.match(/\b\d{12}\b/g) && returned[i].Description.match(/UPC/)){
+//       //store REGEX search for others and store into object
+//       descriptionUPC.push(returned[i]);
+//       returned.splice(returned[i], 1);
+//       i--;
+//     }
+//   };
+// console.log(descriptionUPC.length);
+// console.log(returned.length);
+// console.log(descriptionUPC);
+// console.log(returned);
+// })
 
  module.cpsc = cpsc;
 })(window);
