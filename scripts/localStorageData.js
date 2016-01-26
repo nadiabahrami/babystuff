93f4683733375024e1c5eb636098a9f25909682c
raziyeh bazargan <r.bazargan@gmail.com>
2016-01-25 17:36:27 -0800

    complete local storage

--- scripts/localStorageData.js
+++ scripts/localStorageData.js
@@ -1,20 +1,15 @@
 (function(module){
-
   var retrieveData = [];
-  var getData;
   var objData = {};
   objData.all = [];
-  objData.request = function(){
-    console.log('click');
     $('#btn_search').on('click',function(){
       if (localStorage.searchResult) {
-        retrieveData.push(JSON.parse(localStorage.getItem('searchResult')));
+        var getData = JSON.parse(localStorage.getItem('searchResult'));
         retrieveData.push($('#searchText').val());
         localStorage.setItem('searchResult', JSON.stringify(retrieveData));
       }else {
         localStorage.setItem('searchResult', JSON.stringify($('#searchText').val()));
       }
     });
-  }
   module.objData = objData;
 })(window);
