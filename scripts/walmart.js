b92dac446a33c7d2816443b885866c2953884cfd
Paul Sheridan <paul.sheridan@me.com>
2016-01-26 12:37:27 -0800

    Wal-Mart upc search implimented, not working

--- scripts/walmart.js
+++ scripts/walmart.js
@@ -3,7 +3,7 @@
 
   walmart.all = [];
 
-  walmart.request = function(query) {
+  walmart.similarRequest = function(query) {
     $.ajax({
       type: 'GET',
       url: 'http://api.walmartlabs.com/v1/search?' +
@@ -11,15 +11,27 @@
         '&apiKey=nzqhwruahscwc5ra9kb5uzb6',
       jsonp: 'callback',
       dataType: 'jsonp',
-      data: {
-        categoryId: '5427',
-      },
       success: function(data){
         walmart.all = data;
         console.log(data);
       }
     });
   };
+
+  walmart.upcRequest = function(upc) {
+    $.ajax({
+      type: 'GET',
+      url: 'http://api.walmartlabs.com/v1/search?apiKey=zqhwruahscwc5ra9kb5uzb6&query=' + upc,
+      jsonp: 'callback',
+      dataType: 'jsonp',
+      success: function(data){
+        console.log(data);
+      }
+    });
+  };
+
+
+
 
   module.walmart = walmart;
 })(window);
