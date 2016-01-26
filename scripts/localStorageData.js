6f083946bf6c95d5c20a2ef7203ba3117992e455
raziyeh bazargan <r.bazargan@gmail.com>
2016-01-25 21:19:21 -0800

    add autoComplete for search textBox-

--- scripts/localStorageData.js
+++ scripts/localStorageData.js
@@ -10,7 +10,10 @@
       objData.dataArray.push($('#searchText').val());
       localStorage.setItem('searchResult', JSON.stringify(objData.dataArray));
     }
+    $('#searchText').val('');
   });
+
+  // for autoComplete in search textbox
   var autoCompleteData = JSON.parse(localStorage.getItem('searchResult'));
   $('#searchText').autocomplete({
     source:autoCompleteData,
