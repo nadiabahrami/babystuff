2d4bdea9c4ceab19ce68f36998eacca935d89449
Kate Zurlo-Cuva <zurlocuva@gmail.com>
2016-01-26 14:55:59 -0800

    resultsView.js handlebars display is not finished yet

--- scripts/resultsView.js
+++ scripts/resultsView.js
@@ -3,6 +3,22 @@
 
   resultsView.recallDisplay = function(result){
     if(result === "found"){
+      var template = $("#searches").html();  
+      var compiled = Handlebars.compile(template);  
+
+      var data = {
+          "thumbnailImage": this.thumbnailImage;
+          "name": this.name;
+          "modelNumber": this.modelNumber;
+          "shortDescription": this.shortDescription;
+          "customerRatingImage": this.customerRatingImage;
+        }
+      var renderedHTML = cmpiled(data);
+      return renderedHTML;
+
+    walmart.data
+
+
       //display to user item has been recalled and should not be purchased
       //Run function to display alternates
     }else{
--- scripts/template.js
+++ scripts/template.js
@@ -1,11 +1,8 @@
 (function(module) {
 
 
-var templateData = walmart.all;
-​
- var template = $("#searches").html();  
 
- var compiled = Handlebars.compile (template);  
+​
 
  $(document.body).append(compiled(templateData));
 
