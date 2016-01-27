f5830fea84f27c3cb121baf28629ac07492bf67a
raziyeh bazargan <r.bazargan@gmail.com>
2016-01-26 23:13:34 -0800

    delete some comment from localstorage.js

--- scripts/localStorageData.js
+++ scripts/localStorageData.js
@@ -39,15 +39,6 @@
       });
     }
   }
-
-  // $("#searchText").keypress(function (e) {
-  //  //if the letter is not digit then display error and don't type anything
-  //  if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
-  //     //display error message
-  //     $("#errmsg").html("Digits Only").show().fadeOut("slow").delay(1000);
-  //       return false;
-  //   }
-  // });
   autoCompleteTextBox();
   module.objData = objData;
 })(window);
