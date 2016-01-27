b79056d09083624720996e975c4b42fb6949166c
Kate Zurlo-Cuva <zurlocuva@gmail.com>
2016-01-26 16:04:45 -0800

    Handlebars is working. CPSC data found displays to page.

--- scripts/resultsView.js
+++ scripts/resultsView.js
@@ -1,22 +1,7 @@
 (function(){
   var resultsView= {};
 
-  resultsView.recallDisplay = function(result){
-    if(result === "found"){
-      var template = $("#searches").html();  
-      var compiled = Handlebars.compile(template);  
 
-      var data = {
-          "thumbnailImage": this.thumbnailImage;
-          "name": this.name;
-          "modelNumber": this.modelNumber;
-          "shortDescription": this.shortDescription;
-          "customerRatingImage": this.customerRatingImage;
-        }
-      var renderedHTML = cmpiled(data);
-      return renderedHTML;
-
-    walmart.data
 
 
       //display to user item has been recalled and should not be purchased
